c6825df4cd08ac53c77a8268dc1650282b106632
Nihad <nihadamirov98@gmail.com>
2024-01-06 23:18:12 +0400

    Add Request

--- src/main/java/com/company/service/AccountService.java
+++ src/main/java/com/company/service/AccountService.java
@@ -1,13 +1,22 @@
 package com.company.service;
 
+import com.company.dto.AccountDto;
+import com.company.dto.CreateAccountRequest;
 import com.company.repository.AccountRepository;
+import com.company.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final CustomerService customerService;
 
-    public AccountService(AccountRepository accountRepository) {
+    public AccountService(AccountRepository accountRepository, CustomerService customerService) {
         this.accountRepository = accountRepository;
+        this.customerService = customerService;
+    }
+
+    public AccountDto createAccount(CreateAccountRequest createAccountRequest){
+
     }
 }
